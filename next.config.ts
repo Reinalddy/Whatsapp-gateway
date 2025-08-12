23a0e22278770d84f7fe975eb6986974f8bb7db9
reinalddy <reinalddyaldy183@gmail.com>
2025-08-12 20:50:01 +0700

    fi config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone",
+  experimental: {
+    serverActions: {} // biarin kosong kalau nggak mau set opsi
+  }
 };
 
 export default nextConfig;
